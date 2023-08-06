a7e0f59761b4bfe16ae9d1a47db88eebbaec92a4
Vaishakbg <vaishakbg@gmail.com>
2023-08-06 16:29:29 +0530

    Mongoose, actions and model for onboading

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -20,6 +20,8 @@ import * as z from 'zod';
 import { UserValidation } from "@/lib/validations/user";
 import { isBase64Image } from "@/lib/utils";
 import { useUploadThing } from "@/lib/uploadthing";
+import { updateUser } from "@/lib/actions/user.actions";
+import { usePathname, useRouter } from "next/navigation";
 
 interface Props {
     user: {
@@ -37,27 +39,29 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
 
     const [files, setFiles] = useState<File[]>([]);
     const { startUpload } = useUploadThing("media");
+    const router = useRouter();
+    const pathname = usePathname();
 
     const form = useForm({
         resolver: zodResolver(UserValidation),
         defaultValues: {
             profile_photo: user?.image || "",
-            name:  user?.name || "",
-            username:  user?.username || "",
-            bio:  user?.bio || ""
+            name: user?.name || "",
+            username: user?.username || "",
+            bio: user?.bio || ""
         }
     });
 
-    const handleImage = (e: ChangeEvent<HTMLInputElement>, fieldChange:(value: string) => void) => {
+    const handleImage = (e: ChangeEvent<HTMLInputElement>, fieldChange: (value: string) => void) => {
         e.preventDefault();
 
         const fileReader = new FileReader();
 
-        if(e.target.files && e.target.files.length > 0) {
+        if (e.target.files && e.target.files.length > 0) {
             const file = e.target.files[0];
             setFiles(Array.from(e.target.files));
 
-            if(!file.type.includes('image')) return;
+            if (!file.type.includes('image')) return;
 
             fileReader.onload = async (event) => {
                 const imageDataUrl = event.target?.result?.toString() || '';
@@ -72,53 +76,68 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
 
         const hasImageChanged = isBase64Image(blob);
 
-        if(hasImageChanged) {
-            const imgRes= await startUpload(files);
+        if (hasImageChanged) {
+            const imgRes = await startUpload(files);
 
-            if(imgRes && imgRes[0].fileUrl) {
+            if (imgRes && imgRes[0].fileUrl) {
                 values.profile_photo = imgRes[0].fileUrl;
             }
+        }
+
+        await updateUser({
+            userId: user.id,
+            username: values.username,
+            name: values.name,
+            bio: values.bio,
+            image: values.profile_photo,
+            path: pathname,
+        });
+
+        if(pathname === "/profile/edit") {
+            router.back();
+        } else {
+            router.push('/');
         }
     }
 
     return (
         <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} 
-              className="flex flex-col justify-start gap-10">
+            <form onSubmit={form.handleSubmit(onSubmit)}
+                className="flex flex-col justify-start gap-10">
                 <FormField
                     control={form.control}
                     name="profile_photo"
                     render={({ field }) => (
                         <FormItem className="flex items-center gap-4">
                             <FormLabel className="account-form_image-label">
-                                {field.value?(
-                                    <Image 
-                                      src={field.value}
-                                      alt="Profile photo"
-                                      width={96}
-                                      height={96}
-                                      priority
-                                      className="rounde-full object-contain"
+                                {field.value ? (
+                                    <Image
+                                        src={field.value}
+                                        alt="Profile photo"
+                                        width={96}
+                                        height={96}
+                                        priority
+                                        className="rounde-full object-contain"
                                     />
-                                ): (
-                                    <Image 
-                                      src="/assets/profile.svg"
-                                      alt="Profile photo"
-                                      width={24}
-                                      height={24}
-                                      priority
-                                      className="object-contain"
+                                ) : (
+                                    <Image
+                                        src="/assets/profile.svg"
+                                        alt="Profile photo"
+                                        width={24}
+                                        height={24}
+                                        priority
+                                        className="object-contain"
                                     />
                                 )}
                             </FormLabel>
                             <FormControl className="flex-1 text-base-semibold text-gray-200">
-                                <Input 
-                                  type="file"
-                                  accept="image/*"
-                                  placeholder="Upload a photo"
-                                  className="account-form_image-input"
-                                  onChange={(e)=> handleImage(e, field.onChange)}
-                                  />
+                                <Input
+                                    type="file"
+                                    accept="image/*"
+                                    placeholder="Upload a photo"
+                                    className="account-form_image-input"
+                                    onChange={(e) => handleImage(e, field.onChange)}
+                                />
                             </FormControl>
                         </FormItem>
                     )}
@@ -132,11 +151,11 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
                                 Name
                             </FormLabel>
                             <FormControl >
-                                <Input 
-                                  type="text"
-                                  className="account-form_input no-focus"
-                                  {...field}
-                                  />
+                                <Input
+                                    type="text"
+                                    className="account-form_input no-focus"
+                                    {...field}
+                                />
                             </FormControl>
                         </FormItem>
                     )}
@@ -150,11 +169,11 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
                                 Username
                             </FormLabel>
                             <FormControl>
-                                <Input 
-                                  type="text"
-                                  className="account-form_input no-focus"
-                                  {...field}
-                                  />
+                                <Input
+                                    type="text"
+                                    className="account-form_input no-focus"
+                                    {...field}
+                                />
                             </FormControl>
                         </FormItem>
                     )}
@@ -169,10 +188,10 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
                             </FormLabel>
                             <FormControl>
                                 <Textarea
-                                  rows={10}
-                                  className="account-form_input no-focus"
-                                  {...field}
-                                  />
+                                    rows={10}
+                                    className="account-form_input no-focus"
+                                    {...field}
+                                />
                             </FormControl>
                         </FormItem>
                     )}
---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,45 @@
+"use server"
+
+import { revalidatePath } from "next/cache";
+import User from "../models/user.model";
+import { connectToDB } from "../mongoose";
+
+interface Params {
+    userId: string,
+    username: string,
+    name: string,
+    bio: string,
+    image: string,
+    path: string,
+}
+
+export async function updateUser({
+    userId,
+    username,
+    name,
+    bio,
+    image,
+    path,
+    }: Params): Promise<void> {
+    connectToDB();
+
+    try {
+        await User.findOneAndUpdate(
+            { id: userId },
+            {
+                username: username.toLowerCase(),
+                name,
+                bio,
+                image,
+                onboarded: true,
+            },
+            { upsert: true } //update an existing row if a specified value already exists in a table, and insert a new row if the specified value doesn't already exist.
+        );
+    
+        if(path === '/profile/edit') {
+            revalidatePath(path);
+        } 
+    } catch (error: any) {
+        throw new Error(`Failed to create/update user: ${error.message}`)
+    }
+}
